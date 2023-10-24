87fe853165269bf5cbe0cfb0cd9816285499ddd0
Rechy Missy <74946836+Rachaelmarrion2002@users.noreply.github.com>
2023-10-24 09:33:55 +0300

    implements on signin view model and signin screenstate

--- ELDOCRAFTS/app/src/main/java/screen/signin/SignInScreenState.kt
+++ ELDOCRAFTS/app/src/main/java/screen/signin/SignInScreenState.kt
@@ -1,4 +1,13 @@
 package screen.signin
 
 class SignInScreenState {
+    data class SignInScreenState(
+        val isLoading: Boolean = false
+        val isLoading: Boolean = false,
+        val userName: String = "",
+        val useNameError: String? = null,
+        val password: String = "",
+        val passwordError: String? = null,
+        val isSignInButtonActive: Boolean = false,
+        val navigateToNextScreen: Boolean = false
 }
--- ELDOCRAFTS/app/src/main/java/screen/signin/SignInViewModel.kt
+++ ELDOCRAFTS/app/src/main/java/screen/signin/SignInViewModel.kt
@@ -1,4 +1,69 @@
 package screen.signin
 
 class SignInViewModel {
+    private val signInUseCase: SignInUseCase
+    ): ViewModel() {
+        var  signInScreenState by mutableStateOf(SignInScreenState())
+        fun onAction(action:Action){
+            when(action){
+                is Action.OnResetState ->{
+                    signInScreenState = SignInScreenState()
+                }
+                is Action.OnUserNameChanged ->{
+                    signInScreenState = if (action.userName.isNotEmpty()) {
+                        signInScreenState.copy(userName = action.userName, useNameError = null)
+                    }else{
+                        signInScreenState.copy(useNameError = "")}
+                }
+                is Action.OnPasswordChanged ->{
+                    signInScreenState = if (action.password.isNotEmpty()){
+                        signInScreenState.copy(password = action.password, passwordError = null )
+                    }else{
+                        signInScreenState.copy(passwordError = "")
+                    }
+                }
+                is Action.OnSubmit -> {
+                    signIn()
+                }
+            }
+            signInScreenState = signInScreenState.copy(
+                isSignInButtonActive =
+                signInScreenState.userName.isNotEmpty()
+                        && signInScreenState.useNameError == null
+                        && signInScreenState.password.isNotEmpty()
+                        && signInScreenState.passwordError == null
+            )
+        }
+        private  fun signIn(){
+            val signInRequest = SignInRequest(email = signInScreenState.userName,
+                password = signInScreenState.password
+            )
+            signInScreenState = signInScreenState.copy(
+                isLoading = true
+            )
+            viewModelScope.launch(Dispatchers.IO){
+                signInUseCase.execute(signInRequest).fold(
+                    onSuccess = {
+                        signInScreenState = signInScreenState.copy(
+                            isLoading = false,
+                            navigateToNextScreen = true
+                        )
+                    },
+                    onFailure = {
+                        signInScreenState = signInScreenState.copy(
+                            isLoading = false,
+                            navigateToNextScreen = true
+                        )
+                    }
+                }
+            }
+        }
+
+    }
+}
+sealed interface Action{
+    data class OnUserNameChanged(val userName:String):Action
+    data class OnPasswordChanged(val password:String):Action
+    object OnSubmit:Action
+    object OnResetState:Action
 }
